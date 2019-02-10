9ea6ee30ae8638711ba64b00308838d2b25abe08
prodriguezmartin <pauliitath@gmail.com>
2019-02-10 19:51:22 +0100

    tablas multiplicar

---
+++ multiplicar.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for num1 in range(1,11):
+	print("Tabla del " + str(num1))
+	print("------------------")
+	for num2 in range(1,11):
+		print(str(num1) + " por " + str(num2) + " es " + str(num1*num2))
